b3c0cc0628fb26a4dd5ecb67b0bd95ad468ca0e0
Sara B <saraberg123@gmail.com>
2025-12-17 15:11:16 +0100

    lagt in screenshots för flödesschema och börjat med pseudokoden

---
+++ script.js
@@ -0,0 +1,43 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+START
+SET summa = 0;
+SET total = 0;
+SET person = 0;
+SET dricks = 0;
+
+INPUT "ange total kostnad:" = total;
+INPUT "hur många personer?" = person;
+INPUT "hur mycket dricks?" = dricks;
+
+    IF person > 0 {
+    CALCULATE (total + dricks)/person = summa;
+        PRINT "Varje person ska betala: " + summa;
+    } ELSE {
+        PRINT "Antal personer måste vara mer än 0"
+        }
+    ENDIF
+END
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+START
+SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+SET variabel startOrd till "FOUR";
+SET variabel slutOrd till "FIVE";
+
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+    
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
