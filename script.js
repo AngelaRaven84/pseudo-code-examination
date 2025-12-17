0d2a610d2bd90aa942924a2989f3d3cc43824e77
Sara B <saraberg123@gmail.com>
2025-12-17 17:17:48 +0100

    färdig med pseudokoderna och flowcharten

--- script.js
+++ script.js
@@ -26,18 +26,51 @@ END
 // Startkoden är 100% frivillig och kan tas bort eller skrivas om
 /*
 START
-SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+SET variabel ordbok = ["FOUR", "FOUL", "FOOL", "FOOT", "FORT", "FORE", "FIRE", "FIVE"]; // Innehåller ALLA ord i det engelska språket
 SET variabel startOrd till "FOUR";
 SET variabel slutOrd till "FIVE";
+SET variabel gissadeOrd = [];
+ADD startOrd TO gissadeOrd;
 
+PRINT startOrd;
+PRINT "gissa nästa ord";
 
-function isOneLetterApart(wordOne, wordTwo)
+    FUNCTION isOneLetterApart(wordOne, wordTwo)
+        IF (length(wordOne) !== length(wordTwo)) THEN
+            PRINT "orden måste vara lika långa";    
+            return FALSE;
+            ENDIF
+
     SET variabel diffCount till 0;
 
     // Här behöver du skriva koden för din funktion
-    
+    FOR i = 0 TO length(wordOne) - 1 DO
+        IF (wordOne[i] !== wordTwo[i]) THEN
+        diffCount ++;
+        ENDIF
+    ENDFOR
 
     return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
-end function
 
+    ENDFUNCTION
+
+    WHILE startOrd !== slutOrd DO
+        INPUT gissa
+
+    IF gissa FINNS I ordbok AND
+        gissa FINNS INTE I gissadeOrd AND
+        isOneLetterApart(startOrd, gissa) THEN
+
+        ADD gissa TO gissadeOrd;
+        SET startOrd = gissa;
+        PRINT "Bra! Fortsätt så!";
+
+    ELSE
+        PRINT "tyvärr, fel ord. Försök igen";
+    ENDIF
+    ENDWHILE
+
+    PRINT "Du klarade det! Grattis";
+    PRINT gissadeOrd;
+SLUT
 */
